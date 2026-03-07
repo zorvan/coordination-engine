1fa1a0df86a9ef39fce97735de0e3dca62748d99
zorvan <nonlinear_dynamic@yahoo.com>
2026-03-07 09:08:18 +0300

    fixes

--- bot/start.sh
+++ bot/start.sh
@@ -2,8 +2,16 @@ docker stop coord_bot_db
 sleep 2
 docker system prune --force --volumes
 docker volume prune --force
+
+#export TELEGRAM_TOKEN="xxxxxxxxxx:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+if [ -z "$TELEGRAM_TOKEN" ]; then
+  echo "Error: TELEGRAM_TOKEN environment variable is not set."
+  exit 1
+fi
+
 docker run -d --name coord_bot_db -e POSTGRES_USER=coord_user -e POSTGRES_PASSWORD=coord_pass -e POSTGRES_DB=coord_db -p 5432:5432 --health-cmd="pg_isready -U coord_user -d coord_db"  postgres:15
 sleep 5  # Wait for the database to initialize
 PGPASSWORD=coord_pass psql -h localhost -U coord_user -d coord_db -f db/schema.sql
-#export TELEGRAM_TOKEN="xxxxxxxxxx:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-python3 main.py
+python3 main.py > bot.log 2>&1 &
+echo "******* Bot started. Logs are being written to bot.log *******"
+tail -f bot.log
